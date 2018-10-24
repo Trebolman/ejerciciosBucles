1609fbecf67a3f2f9cfebb558e04aec6883044fe
Daniel Isaac <trebolman@gmail.com>
2018-10-24 17:07:24 -0500

    Creacion del proyecto "ejerciciosBucles"

---
+++ ejerciciosBucles/ejerciciosBucles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejerciciosBucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
